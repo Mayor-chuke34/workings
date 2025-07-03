6d6bb8050cfa6a579129f89fc93b2adb5707ce83
mayorchuke34 <chukwukao@gmail.com>
2025-07-03 12:09:34 +0100

    workings

---
+++ index.js
@@ -0,0 +1,48 @@
+const  express = require ('express');
+const  mongoose = require ('mongoose') ;
+const Product = require('./models/product.model.js');
+
+const app = express();
+const PORT = 3000;
+app.use (express.json ());
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express is running!');
+});
+
+
+app.post('/api/products',async (req, res) =>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    };
+});
+
+    app.get('/api/products',async (req, res) =>{
+    try {
+        const product = await Product.find(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    };
+});
+
+mongoose.connect('mongodb+srv://chukwuka:C2IM3zyClhH0aoCq@cluster0.jrglgbx.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0 ')
+.then (() => {
+    console.log("connected to database!");
+})
+.catch(() => {
+    console.log("connected failed")
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from home");''
+});
