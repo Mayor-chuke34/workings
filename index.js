dd5a55e44d790ef138128d1c35675e13f81dc388
mayorchuke34 <chukwukao@gmail.com>
2025-07-04 13:08:15 +0100

    Added users.model.js in model.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const  express = require ('express');
 const  mongoose = require ('mongoose') ;
 const Product = require('./models/product.model.js');
+const Users = require('./models/users.model.js');
 
 const app = express();
 const PORT = 3000;
@@ -29,7 +30,7 @@ app.post('/api/products',async (req, res) =>{
     };
 });
 
-app.get('/api/products/:id', async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
     try {
         const {id} = req.params;
         const product = await Product.findById(id);
@@ -38,6 +39,88 @@ app.get('/api/products/:id', async (req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+
+app.put("/api/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+        
+        const updatedproduct = await Product.findById(id);
+        res.status(200).json(updatedproduct)
+    } catch (error) {
+        res.status(200).json({message: error.message});
+    } 
+});
+
+app.delete("/api/products/id", async (req,res) => {
+    try{
+        const {id} = req.params;
+        const product = await product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({message: "product not found"});
+        }
+        const deleteproduct = await Product.findById(id);
+        res.status(200).json(deleteproduct)
+    } catch (error) {
+        res.status(200).json({message: error.message});
+    } 
+});
+
+
+app.post('/api/users',async (req, res) =>{
+    try {
+        const users = await Users.create(req.body);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    };
+});
+
+app.get('/api/users',async (req, res) =>{
+    try {
+        const users = await users.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    };
+});
+
+app.put("/api/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const users = await Users.findByIdAndUpdate(id, req.body);
+
+        if (!users) {
+            return res.status(404).json({message: "users not found"});
+        }
+        
+        const updatedusers = await users.findById(id);
+        res.status(200).json(updatedusers)
+    } catch (error) {
+        res.status(200).json({message: error.message});
+    } 
+});
+
+app.delete("/api/users/id", async (req,res) => {
+    try{
+        const {id} = req.params;
+        const users = await users.findByIdAndDelete(id);
+        if (!users) {
+            return res.status(404).json({message: "users not found"});
+        }
+        const deleteproduct = await users.findById(id);
+        res.status(200).json(deleteusers)
+    } catch (error) {
+        res.status(200).json({message: error.message});
+    } 
+});
+
+
 
 mongoose.connect('mongodb+srv://chukwuka:C2IM3zyClhH0aoCq@cluster0.jrglgbx.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0 ')
 .then (() => {
---
+++ models/users.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter your name"], 
+        },
+
+        username: {
+            type: String,
+            required: [true, "Please enter your username"],
+            unique: true,
+        },
+
+        Email: {
+            type: String,
+            required: [true, "Please enter your email"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please enter your password"],
+        },
+        number: {
+            type: String,
+            required: [true, "Please enter your phone number"],
+        },
+    }
+)
+
+const Users = mongoose.model("users", UsersSchema);
+
+module.exports = Users;
