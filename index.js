33321abe115489c526e938d9d0c69dc7a9f10fc2
mayorchuke34 <chukwukao@gmail.com>
2025-07-04 06:46:20 +0100

    modified line 25

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/api/products',async (req, res) =>{
 
     app.get('/api/products',async (req, res) =>{
     try {
-        const product = await Product.find(req.body);
+        const product = await Product.find({});
         res.status(200).json(product);
     } catch (error) {
         res.status(500).json({message:error.message});
@@ -53,6 +53,3 @@ app.listen(PORT, () => {
 });
 
 
-app.get("/", (req, res) => {
-    res.send("Hello from home");''
-});
